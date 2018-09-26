59d7c013d9e8682fe100159bc3b26dfefcb27f85
Aaron Franke <arnfranke@yahoo.com>
2018-09-26 13:08:35 -0400

    Add script for only Ninite apps and version patches
    And slim down the list of Ninite apps. For quick setup of a computer.

---
+++ Ninite And Patches Only.bat
@@ -0,0 +1,89 @@
+
+@echo off
+
+for /f "tokens=4-7 delims=[.] " %%i in ('ver') do (if %%i==Version (set version=%%j.%%k) else (set version=%%i.%%j))
+
+echo This tool will automatically download many programs and patches.
+echo.
+echo A lot of the programs are within a Ninite wrapper for easy installation.
+echo.
+echo Note: Not all included links are opened by this script. 
+echo Visit the folders to manually download other programs.
+echo.
+if "%version%" == "5.0" echo  Detected Windows version: Windows 2000 (unsupported)
+if "%version%" == "5.1" echo  Detected Windows version: Windows XP
+if "%version%" == "5.2" echo  Detected Windows version: Windows XP x64 (unsupported)
+if "%version%" == "6.0" echo  Detected Windows version: Windows Vista (unsupported)
+if "%version%" == "6.1" echo  Detected Windows version: Windows 7
+if "%version%" == "6.2" echo  Detected Windows version: Windows 8.0 (unsupported)
+if "%version%" == "6.3" echo  Detected Windows version: Windows 8.1
+if "%version%" == "10.0" echo  Detected Windows version: Windows 10
+echo.
+
+rem Out-of-support warnings for various EOL Windows versions. 
+if "%version%" == "5.0" echo WINDOWS 2000 USERS PLEASE READ THIS!
+if "%version%" == "5.0" echo.
+if "%version%" == "5.0" echo Windows 2000 is no longer supported. Please consider switching to Lubuntu.
+
+if "%version%" == "5.1" echo WINDOWS XP USERS PLEASE READ THIS!
+if "%version%" == "5.1" echo.
+if "%version%" == "5.1" echo I highly recommend that you install the Unofficial Service Pack 4, for 
+if "%version%" == "5.1" echo Windows XP 32-bit systems only. Includes updates through April 2019 from 
+if "%version%" == "5.1" echo POSReady 2009. Please ensure Service Pack 3 is installed prior to this.
+if "%version%" == "5.1" echo.
+if "%version%" == "5.1" echo Please consider switching to Lubuntu.
+
+if "%version%" == "5.2" echo WINDOWS XP X64 USERS PLEASE READ THIS!
+if "%version%" == "5.2" echo.
+if "%version%" == "5.2" echo Windows XP x64 edition is no longer supported. Only the 32-bit version 
+if "%version%" == "5.2" echo is supported with Unofficial SP4 until April 2019. 
+if "%version%" == "5.2" echo.
+if "%version%" == "5.2" echo Please consider switching to Lubuntu.
+
+if "%version%" == "6.0" echo WINDOWS VISTA USERS PLEASE READ THIS!
+if "%version%" == "6.0" echo.
+if "%version%" == "6.0" echo Windows Vista is no longer supported. Please consider switching to Xubuntu.
+
+if "%version%" == "6.2" echo WINDOWS 8.0 USERS PLEASE READ THIS!
+if "%version%" == "6.2" echo.
+if "%version%" == "6.2" echo Windows 8.0 is no longer supported. Please upgrade to Windows 8.1 or Xubuntu.
+
+echo.
+
+pause
+
+cd "Software"
+echo.
+echo Beginning automatic download of software...
+echo.
+
+echo Downloading many programs from Ninite...
+    "Ninite Apps.url"
+
+echo.
+pause
+echo.
+echo.
+
+cd "Version-Specific Patches"
+    if "%version%" == "5.1" "Win XP - .NET Framework 4.0.url"
+    if "%version%" == "5.1" "Win XP - Unofficial Service Pack 4.url"
+    if "%version%" == "5.1" "Win XP - User Profile Hive Cleanup Service.url"
+    if "%version%" == "6.1" "Win 7 - Microsoft Security Essentials.url"
+    if "%version%" == "6.2" "Win 8 - Classic Shell.url"
+    if "%version%" == "6.3" "Win 8 - Classic Shell.url"
+    if "%version%" == "10.0" start "Win10 Decrapifier PowerShell Script" PowerShell.exe -ExecutionPolicy Bypass -File "Win 10 - Decrapifier v2.ps1"
+cd ..
+
+echo.
+pause
+echo.
+echo.
+
+cls
+echo Done!
+echo.
+
+pause
+
+
