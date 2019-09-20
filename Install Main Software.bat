629161e17aac63809006fc2a618eee509ecbe1c5
Aaron Franke <arnfranke@yahoo.com>
2019-09-20 17:35:07 -0400

    Update for 2019
    Change EOL notices, rename files, add RTIU registry key

--- Install Main Software.bat
+++ Install Main Software.bat
@@ -12,11 +12,11 @@ echo Note: Not all included links are opened by this script.
 echo Visit the folders to manually download other programs.
 echo.
 if "%version%" == "5.0" echo  Detected Windows version: Windows 2000 (unsupported)
-if "%version%" == "5.1" echo  Detected Windows version: Windows XP
-if "%version%" == "5.2" echo  Detected Windows version: Windows XP x64 (unsupported)
-if "%version%" == "6.0" echo  Detected Windows version: Windows Vista (unsupported)
+if "%version%" == "5.1" echo  Detected Windows version: Windows XP (unsupported by Microsoft)
+if "%version%" == "5.2" echo  Detected Windows version: Windows XP x64 (unsupported by Microsoft)
+if "%version%" == "6.0" echo  Detected Windows version: Windows Vista (unsupported by Microsoft)
 if "%version%" == "6.1" echo  Detected Windows version: Windows 7
-if "%version%" == "6.2" echo  Detected Windows version: Windows 8.0 (unsupported)
+if "%version%" == "6.2" echo  Detected Windows version: Windows 8.0 (unsupported by Microsoft)
 if "%version%" == "6.3" echo  Detected Windows version: Windows 8.1
 if "%version%" == "10.0" echo  Detected Windows version: Windows 10
 echo.
@@ -25,19 +25,19 @@ rem Out-of-support warnings for various EOL Windows versions.
 if "%version%" == "5.0" echo WINDOWS 2000 USERS PLEASE READ THIS!
 if "%version%" == "5.0" echo.
 if "%version%" == "5.0" echo Windows 2000 is no longer supported. Please consider switching to Lubuntu.
+if "%version%" == "5.0" echo This script will probably not work correctly on Windows 2000.
 
 if "%version%" == "5.1" echo WINDOWS XP USERS PLEASE READ THIS!
+if "%version%" == "5.2" echo.
+if "%version%" == "5.2" echo Windows XP is no longer supported. Please consider switching to Lubuntu.
 if "%version%" == "5.1" echo.
-if "%version%" == "5.1" echo I highly recommend that you install the Unofficial Service Pack 4, for 
-if "%version%" == "5.1" echo Windows XP 32-bit systems only. Includes updates through April 2019 from 
-if "%version%" == "5.1" echo POSReady 2009. Please ensure Service Pack 3 is installed prior to this.
-if "%version%" == "5.1" echo.
-if "%version%" == "5.1" echo Please consider switching to Lubuntu.
+if "%version%" == "5.1" echo If that's not an option, I highly recommend that you install Unofficial
+if "%version%" == "5.1" echo Service Pack 4. Includes updates through April 2019 from POSReady 2009.
+if "%version%" == "5.1" echo Please ensure Service Pack 3 is installed prior to this.
 
 if "%version%" == "5.2" echo WINDOWS XP X64 USERS PLEASE READ THIS!
 if "%version%" == "5.2" echo.
 if "%version%" == "5.2" echo Windows XP x64 edition is no longer supported. Please consider switching to Lubuntu.
-if "%version%" == "5.2" echo Only the 32-bit version is supported with Unofficial SP4 until April 2019. 
 
 if "%version%" == "6.0" echo WINDOWS VISTA USERS PLEASE READ THIS!
 if "%version%" == "6.0" echo.
@@ -75,12 +75,8 @@ pause
 echo.
 echo.
 
-cd "Misc Libraries"
-echo Downloading Misc Libraries and Patches...
-    "Ext2Fsd.url"
-cd ..
-
-cd "Version-Specific Patches"
+cd "Patches"
+    realtimeisuniversal.reg
     if "%version%" == "5.1" "Win XP - .NET Framework 4.0.url"
     if "%version%" == "5.1" "Win XP - Unofficial Service Pack 4.url"
     if "%version%" == "5.1" "Win XP - User Profile Hive Cleanup Service.url"
